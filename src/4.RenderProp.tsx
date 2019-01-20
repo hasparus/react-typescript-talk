694bba3e2920b0519c34a79d8301ab2829fd05a0
Piotr Pietrzak <hasparus@gmail.com>
2019-01-20 22:32:12 +0100

    Add Solar System render prop example

--- src/4.RenderProp.tsx
+++ src/4.RenderProp.tsx
@@ -1,33 +1,52 @@
 /** @jsx jsx */
-import { css, jsx } from "@emotion/core";
+import { css, jsx, keyframes } from "@emotion/core";
 import React from "react";
+import { Spring, config } from "react-spring";
 
 import { render } from "./talkUtils";
 
-type MousePosition = {
+type PointerPosition = {
   x: number;
   y: number;
 };
 
 type Props = {
-  children: (_: MousePosition) => React.ReactNode;
+  children: (_: PointerPosition) => React.ReactNode;
 };
 
 type State = {
-  mousePosition?: MousePosition;
+  mousePosition?: PointerPosition;
 };
 
-class MouseProvider extends React.Component<Props, State> {
+class PointerProvider extends React.Component<Props, State> {
   state: State = {};
-  componentDidMount() {}
 
-  componentWillUnmount() {}
+  componentDidMount() {
+    document.addEventListener("pointermove", this.handlePointerMove);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener(
+      "pointermove",
+      this.handlePointerMove
+    );
+  }
+
+  handlePointerMove = (event: PointerEvent) => {
+    this.setState({
+      mousePosition: {
+        x: event.x,
+        y: event.y,
+      },
+    });
+  };
+
   render() {
     const { children } = this.props;
     const { mousePosition } = this.state;
 
     if (!mousePosition) {
-      return null;
+      return "No pointer";
     }
 
     return children(mousePosition);
@@ -35,33 +54,128 @@ class MouseProvider extends React.Component<Props, State> {
 }
 
 render(
-  <MouseProvider>
-    {({ x, y }) => (
+  <PointerProvider>
+    {pointerPosition => (
       <Stage>
-        <Sun x={x} y={y} />
+        <Spring to={pointerPosition} config={config.gentle}>
+          {({ x, y }) => (
+            <SolarSystem
+              x={x}
+              y={y}
+              isMoving={
+                pointerPosition.x !== x || pointerPosition.y !== y
+              }
+            >
+              <Earth />
+            </SolarSystem>
+          )}
+        </Spring>
       </Stage>
     )}
-  </MouseProvider>
+  </PointerProvider>
 );
 
 const Stage = (props: React.ComponentProps<"div">) => (
   <div
     css={css`
+      position: absolute;
+      left: 0;
+      top: 0;
       width: 100%;
       height: 100%;
+      background: #111;
+      cursor: none;
+      user-select: none;
+      overflow: hidden;
     `}
+    {...props}
   />
 );
 
-const Sun = ({ x, y }) => (
+type SolarSystemProps = {
+  x: number;
+  y: number;
+  children?: React.ReactNode;
+} & SunProps;
+
+const SolarSystem = ({
+  x,
+  y,
+  isMoving,
+  children,
+}: SolarSystemProps) => (
   <div
     style={{
       position: "absolute",
-      transform: `translate3d(${x}px, ${y}px, 0)`,
+      transform: `translate3d(
+        calc(${x}px - 50%),
+        calc(${y}px - 50%), 
+        0)`,
+    }}
+  >
+    <Sun isMoving={isMoving} />
+    {children}
+  </div>
+);
+
+type SunProps = { isMoving: boolean };
+const Sun = ({ isMoving }: SunProps) => (
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "50px",
+      lineHeight: 1,
+      transformOrigin: "center",
+      transform: `translate3d(-50%, -50%, 0) ${
+        isMoving ? "" : `rotateZ(${Math.random() * 360}deg)`
+      }`,
     }}
   >
     🌞
   </div>
 );
 
-const Earth = () => <div>🌎</div>;
+const orbit = keyframes`
+  0% {
+    transform: rotateZ(0deg);
+  }
+
+  100% {
+    transform: rotateZ(-360deg);
+  }
+`;
+
+const inverseOrbit = keyframes`
+  0% {
+    transform: rotateZ(0deg);
+  }
+
+  100% {
+    transform: rotateZ(360deg);
+  }
+`;
+
+const Earth = () => (
+  <div
+    css={css`
+      position: absolute;
+      left: 100px;
+      top: 100px;
+      transform-origin: -100px -100px;
+      animation: ${orbit} 10s linear infinite;
+    `}
+  >
+    <div
+      css={css`
+        animation: ${inverseOrbit} 10s linear infinite;
+      `}
+    >
+      🌎
+    </div>
+  </div>
+);
+
+// Workaround for CodeSandbox
+(window as any)._jsx = jsx;
